5ee852c714af8e6bc8f1bdf0b8d17d49be68b647
chethaka <csathmal24@gamil.com>
2024-02-06 16:28:07 +0530

    changed-the-logic

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def main():
         now = datetime.now()
         work_time = timedelta(minutes=20)
         rest_time = timedelta(minutes=5)
-        if work_time == timedelta(now - end_point):
+        if work_time <= now - end_point:
             end_point += work_time
             notification.notify(
                 title = '5 minure break',
                 message = 'Take a break now', 
                 timeout = 5
             )
-        elif rest_time == timedelta(now - end_point):
+        elif rest_time <= now - end_point:
             end_point += rest_time
             notification.notify(
                 title = 'Start work now', 
