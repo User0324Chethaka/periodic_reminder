0eeaeaf1fc0880cc8fd06b249238bbf83a131008
chethaka <csathmal24@gamil.com>
2024-02-06 17:57:02 +0530

    works-now-need-to-solve-python-dbus-package-error

--- main.py
+++ main.py
@@ -3,24 +3,27 @@ from datetime import datetime, timedelta
 
 
 def main():
+    print('started')
     end_point = datetime.now()
     while True:
         now = datetime.now()
         work_time = timedelta(minutes=20)
         rest_time = timedelta(minutes=5)
         if work_time <= now - end_point:
+            print('work break now')
             end_point += work_time
             notification.notify(
                 title = '5 minure break',
                 message = 'Take a break now', 
-                timeout = 5
+                timeout = 10
             )
         elif rest_time <= now - end_point:
+            print('break finishes now')
             end_point += rest_time
             notification.notify(
                 title = 'Start work now', 
                 message = 'Break over: start work',
-                timeout = 5
+                timeout = 10
             )
 
 
