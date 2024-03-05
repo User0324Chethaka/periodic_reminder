8c94d75ae4e6839176ccf1ed4433d6daa1cbb944
chethaka <csathmal24@gamil.com>
2024-03-05 17:46:23 +0530

    commiting before rechecking the program

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from plyer import notification
 from datetime import datetime, timedelta
+import json
+import sys
 
 
 def main():
-    print('started')
+    user_input_file = r"./user_input.json"
+    work_time_min = None #  Time to work in int
+    rest_time_min = None #  Time to rest in int
+    with open(user_input_file, "r") as jf:
+        f = json.load(jf)
+        work_time_min = f["work_time"]
+        rest_time_min = f["rest_time"]
+
+    # No user input notification / warning 
+    if work_time_min == '' or rest_time_min == '':
+        notification.notify(
+            title = 'WARNING !!!',
+            message = 'No user input provided ',
+            timeout = 20
+        )
+        sys.exit()
+
     end_point = datetime.now()
     while True:
         now = datetime.now()
-        work_time = timedelta(minutes=20)
-        rest_time = timedelta(minutes=5)
+        work_time = timedelta(minutes=work_time_min) # Work time as datetime
+        rest_time = timedelta(minutes=rest_time_min) # Rest tiem as datetime
         if work_time <= now - end_point:
-            print('work break now')
             end_point += work_time
             notification.notify(
                 title = '5 minure break',
@@ -18,7 +35,6 @@ def main():
                 timeout = 10
             )
         elif rest_time <= now - end_point:
-            print('break finishes now')
             end_point += rest_time
             notification.notify(
                 title = 'Start work now', 
